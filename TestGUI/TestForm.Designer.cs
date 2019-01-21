1a6410568197f1f87723a25743d0c78ecc803f11
amezzolesta <amezzolesta@gmail.com>
2019-01-20 19:54:23 -0500

    project commit

---
+++ TestGUI/TestForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TestGUI
+{
+    partial class TestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTest = new System.Windows.Forms.Button();
+            this.txtBoxTest = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(13, 13);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // txtBoxTest
+            // 
+            this.txtBoxTest.Location = new System.Drawing.Point(13, 43);
+            this.txtBoxTest.Name = "txtBoxTest";
+            this.txtBoxTest.Size = new System.Drawing.Size(409, 389);
+            this.txtBoxTest.TabIndex = 1;
+            this.txtBoxTest.Text = "";
+            // 
+            // TestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 444);
+            this.Controls.Add(this.txtBoxTest);
+            this.Controls.Add(this.btnTest);
+            this.Name = "TestForm";
+            this.Text = "Test Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.RichTextBox txtBoxTest;
+    }
+}
+
---
+++ TestGUI/TestForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using X12translator;
+
+namespace TestGUI
+{
+    public partial class TestForm : Form
+    {
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtBoxTest.Text += "start process";
+
+                string[] x12DocInfo = new string[0];
+                string EOL = "~";
+                char delimiter = '*';
+
+                X12.TranslateX12(x12DocInfo, EOL, delimiter);
+
+            }
+            catch(Exception ex)
+            {
+                //return errors
+            }
+        }
+    }
+}
